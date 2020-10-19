2562413621a1dad4b933768d29adad510b6d241c
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-10-19 15:58:30 +0300

    Add main page

--- frontend/src/containers/Routing/index.jsx
+++ frontend/src/containers/Routing/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Auth from 'scenes/Auth/containers/Auth';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Auth from 'scenes/Auth';
+import Main from 'scenes/Main';
 
 const Routing = () => {
   return (
     <Switch>
+      <Route exact path='/' component={Main} />
       <Route exact path='/auth/:page' component={Auth} />
+      <Redirect to='/' />
     </Switch>
   );
 };
